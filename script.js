d340c68b39427ac62c5c407dd325a715b0c1c24a
Esheq12 <a7lam1231@outlook.com>
2026-07-28 02:58:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,23 @@
 // الإعدادات
 // =====================================
 
+const api = "https://script.google.com/macros/s/AKfycbzbj-n1Bew61qx5_8N7PCdvZGQf8PKjbIEDqhMRphK5kLyctGz3iIhWa_oN7wG_QG0BaQ/exec";
 
+fetch(api)
+.then(res => res.json())
+.then(data => {
+
+    products = data.filter(p => p.Status === "متوفر");
+
+    filtered = [...products];
+
+    loadCategories();
+
+    renderProducts();
+
+    updateCartBar();
+
+});
 
 // السلة
 
