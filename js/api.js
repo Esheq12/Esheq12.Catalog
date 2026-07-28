f5c0fcd40f0c9c49ad29fd0e9e329c77890542cc
Esheq12 <a7lam1231@outlook.com>
2026-07-28 15:09:34 +0300

    Fix formatting in loadProducts functionRefactor: Split script into modules

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const api = "https://script.google.com/macros/s/AKfycbzbj-n1Bew61qx5_8N7PCdvZGQf8PKjbIEDqhMRphK5kLyctGz3iIhWa_oN7wG_QG0BaQ/exec";
 
 function loadProducts() {
-
+    
     fetch(api)
     .then(res => res.json())
     .then(data => {
