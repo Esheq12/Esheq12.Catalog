db685d2fea5f0e1c9e8b8d8709ce8e86d09d35d6
Esheq12 <a7lam1231@outlook.com>
2026-08-13 18:25:34 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 const api = "https://script.google.com/macros/s/AKfycbzbj-n1Bew61qx5_8N7PCdvZGQf8PKjbIEDqhMRphK5kLyctGz3iIhWa_oN7wG_QG0BaQ/exec";
 
 function loadProducts() {
-    
+
     fetch(api)
     .then(res => res.json())
     .then(data => {
@@ -15,6 +15,25 @@ function loadProducts() {
         renderProducts();
 
         updateCartBar();
+
+        // فتح المنتج إذا كان الرابط يحتوي على product ID
+        const params = new URLSearchParams(window.location.search);
+
+        const productID = params.get("product");
+
+        if(productID){
+
+            const product = products.find(
+                p => String(p.ID) === String(productID)
+            );
+
+            if(product){
+
+                openProduct(product);
+
+            }
+
+        }
 
     });
 
